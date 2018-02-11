0428bdd63663042a4ccc50d542e1af4ae755acb6
Johannes Kronmüller <Johannes.Kronmueller@gmail.com>
2018-02-11 14:01:28 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['transform-class-properties', 'react-hot-loader/babel'],
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+          }
+        ]
+      },
+      plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
+        title: 'React / Webpack 4 / Babel 7 / Hot Reloading / Boilerplate',
+      })],
+}
